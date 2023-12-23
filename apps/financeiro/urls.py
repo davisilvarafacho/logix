e5e8a69e34663ce0b7f68c06577b982316173403
rafacho <davi.s.rafacho.developer@gmail.com>
2023-12-23 17:54:59 -0300

    feat: desabilitação das views

--- apps/financeiro/urls.py
+++ apps/financeiro/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SaidaDinheiroViewSet, EntradaDinheiroViewSet
+# from .views import SaidaDinheiroViewSet, EntradaDinheiroViewSet
 
 router = DefaultRouter()
-router.register('despesas', SaidaDinheiroViewSet, basename='despesas')
-router.register('entradas', EntradaDinheiroViewSet, basename='entradas')
+# router.register('despesas', SaidaDinheiroViewSet, basename='despesas')
+# router.register('entradas', EntradaDinheiroViewSet, basename='entradas')
 
 urlpatterns = [
     path('', include(router.urls)),
