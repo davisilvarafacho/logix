5b1c6b9a993bef520916b8ec800773ff52ca1176
rafacho <davi.s.rafacho.developer@gmail.com>
2024-01-06 13:23:33 -0300

    feat: adição da importação e exportação + actions de utilidade

--- apps/financeiro/admin.py
+++ apps/financeiro/admin.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
+from django.db.models.aggregates import Sum
 from django.utils.translation import gettext_lazy as _
+from django.contrib import messages
+from django.contrib import admin
 
-from unfold.admin import ModelAdmin
 from import_export.admin import ImportExportModelAdmin
+from unfold.admin import ModelAdmin
 from unfold.contrib.import_export.forms import ExportForm, ImportForm
 
 from apps.financeiro.models import (
@@ -26,16 +28,17 @@ class EntradaDinheiroAdmin(ModelAdmin):
 class SaidaDinheiroAdmin(ModelAdmin, ImportExportModelAdmin):
     import_form_class = ImportForm
     export_form_class = ExportForm
+
     list_display = (
         "descricao",
-        "paga",
+        "categoria",
         "valor_total",
-        "entrada",
+        "paga",
+        "destino",
         "obrigatoria",
         "parcela",
         "total_parcelas",
-        "categoria",
-        "data_gasto",
+        "entrada",
     )
 
     fieldsets = (
@@ -49,6 +52,7 @@ class SaidaDinheiroAdmin(ModelAdmin, ImportExportModelAdmin):
                 )
             },
         ),
+        (_("Pagamento"), {"fields": ("entrada", "destino")}),
         (_("Classificações"), {"fields": ("categoria",)}),
         (
             _("Recorrência"),
@@ -64,24 +68,58 @@ class SaidaDinheiroAdmin(ModelAdmin, ImportExportModelAdmin):
             {
                 "fields": (
                     "paga",
-                    "entrada",
                     "obrigatoria",
                 )
             },
         ),
     )
 
-    list_filter = (
-        "categoria",
-        "categoria__tipo",
-        "entrada",
-        "obrigatoria",
-        "paga",
-    )
-
+    list_filter = ("entrada",)
     ordering = ("-id",)
     search_fields = ("descricao",)
     exclude = ("data_hora_criacao", "data_hora_atualizacao", "ativo")
+
+    actions = (
+        "marcar_como_paga",
+        "marcar_como_nao_paga",
+        "duplicar_saida",
+        "calcular_total_gastos",
+    )
+
+    @admin.action(description=_("Marcar como paga"))
+    def marcar_como_paga(cls, request, queryset):
+        for saida in queryset:
+            saida.paga = True
+            saida.save()
+
+    @admin.action(description=_("Marcar como não paga"))
+    def marcar_como_nao_paga(cls, request, queryset):
+        for saida in queryset:
+            saida.paga = False
+            saida.save()
+
+    @admin.action(description=_("Duplicar saída"))
+    def duplicar_saida(cls, request, queryset):
+        for saida in queryset:
+            saida.pk = None
+            saida.paga = False
+
+            if saida.parcela and saida.parcela < saida.total_parcelas:
+                saida.parcela += 1
+
+            saida.save()
+
+    @admin.action(description=_("Calcular total dos gastos"))
+    def calcular_total_gastos(cls, request, queryset):
+        valor_total_gastos = queryset.aggregate(valor_total_gastos=Sum("valor_total"))[
+            "valor_total_gastos"
+        ]
+        valor_total_gastos = round(valor_total_gastos, 2) if valor_total_gastos else 0
+        valor_total_gastos = str(valor_total_gastos).replace(".", ",")
+
+        messages.add_message(
+            request, messages.INFO, f"O total de gastos é: R$ {valor_total_gastos}"
+        )
 
 
 @admin.register(CategoriaGasto)
